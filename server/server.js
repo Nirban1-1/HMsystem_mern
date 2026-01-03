476d651483f53b12315da252b2c4097e1d67ad5d
Nirban <nirban.roy@g.bracu.ac.bd>
2026-01-04 04:30:18 +0600

    a

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/connectDB.js';
 import mongoose from 'mongoose';
-
+import cors from "cors";
 
 import userRoutes from './routes/userRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
@@ -26,15 +26,13 @@ connectDB();
 const app = express();
 
 // CORS middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://hmsystem-mern.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 
